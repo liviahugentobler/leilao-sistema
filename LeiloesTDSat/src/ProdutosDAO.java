8f6a5f5601b75c356251548668ef136c4196713e
livia.hugentobler <livia.hugentobler@gmail.com>
2026-03-12 09:34:51 -0300

    feat: implementada funcionalidade salvar e mensagens de feedback ao usuário

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -24,31 +24,31 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-//    public void cadastrarProduto (ProdutosDTO produto){
-//    conn = new conectaDAO().connectDB(); 
-//    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
-//    
-//    try {
-//        prep = conn.prepareStatement(sql);
-//        prep.setString(1, produto.getNome());
-//        prep.setInt(2, produto.getValor());
-//        prep.setString(3, produto.getStatus());
-//        
-//        prep.executeUpdate(); 
-//        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
-//        
-//    } catch (SQLException erro) {
-//        JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + erro.getMessage());
-//    } finally {
-//        try {
-//            conn.close(); 
-//        } catch (SQLException e) {
-//     
-//        }
-//    }
+    public void cadastrarProduto (ProdutosDTO produto){
+    conn = new conectaDAO().connectDB(); 
+    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+    
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        
+        prep.executeUpdate(); 
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+        
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + erro.getMessage());
+    } finally {
+        try {
+            conn.close(); 
+        } catch (SQLException e) {
+     
+        }
+    }
 }
     
-//    public ArrayList<ProdutosDTO> listarProdutos(){
+    public ArrayList<ProdutosDTO> listarProdutos(){
         
         return listagem;
     }
